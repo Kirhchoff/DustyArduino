a92846568fe18be87553da8ad707f939decbf400
Krzysztof Kirsz <k.kirsz@gmail.com>
2017-04-06 21:56:10 +0200

    V0 measuring - #2 Medium version

--- DustyArduino/DustyArduino.ino
+++ DustyArduino/DustyArduino.ino
@@ -1,20 +1,31 @@
 const int iled = 13;
 const int aout = A1;
 const int measurementDelay = 280;
-int aoutValue = 0;
+int minV = 5000;
+
 void setup() {
   pinMode(iled, OUTPUT);
   digitalWrite(iled, LOW); //iled OFF by default
   Serial.begin(9600); //initialize serial port
 }
+
+int toVoltage(int analogValue)
+{
+  const int refVoltage = 5000;
+  return int((analogValue*11) * (refVoltage/1023.0));
+}
 void loop() {
+  int aoutValue = 0;
   digitalWrite(iled, HIGH);
   delayMicroseconds(measurementDelay);
-  aoutValue = analogRead(aout) * 11;
+  aoutValue = analogRead(aout);
   digitalWrite(iled, LOW);
-  
-  Serial.print(aoutValue);
-  Serial.print(" ");
-  
+
+  int v = toVoltage(aoutValue);
+  if (v<minV) {
+    minV = v;
+    Serial.print(minV);
+    Serial.print(" ");
+  }
   delay(1000);
 }
